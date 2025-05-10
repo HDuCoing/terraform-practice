7d2557289330911ce2bf62c6b8d9f26971b89f91
Holly <hollyducoing@gmail.com>
2025-05-10 17:06:26 +1200

    added variables

--- variables.tf
+++ variables.tf
@@ -2,3 +2,37 @@ variable "instance_type" {
   description = "Type of EC2 instance to provision"
   default     = "t3.nano"
 }
+variable "ami_filter" {
+  description = "AMI filter for the application"
+
+  type = object({
+    name = string
+    owners = string
+  })
+  default = {
+    name = ["bitnami-tomcat-*-x86_64-hvm-ebs-nami"]
+    owners = ["979382823631"] # Bitnami
+  }
+}
+variable "environment" {
+  description = "Environment name"
+  type = object({
+    name = string
+    network_prefix = string
+  })
+  default = {
+    name = "dev"
+    network_prefix = "10.0"
+  }
+}
+variable "min_size" {
+  description = "Minimum size of the Auto Scaling group"
+  type        = number
+  default     = 1
+}
+variable "max_size" {
+  description = "Maximum size of the Auto Scaling group"
+  type        = number
+  default     = 2
+}
+ 
